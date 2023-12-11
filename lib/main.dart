7f512872d4c1742cfd14bccbf66f5bee728200f1
AnanasName <martin25102002@gmail.com>
2023-12-12 00:38:19 +0300

    DetailsPageChanges

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:weatherapp/pages/weather_details_page.dart';
-import 'pages/home_page.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(WeatherForecast());
@@ -67,69 +65,7 @@ class WeatherForecast extends StatelessWidget {
   void _onCardPressed(BuildContext context, int index) {
     Navigator.push(
       context,
-      MaterialPageRoute(builder: (context) => WeatherDetailsPage()),
-    );
-  }
-}
-
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
-  }
-}
-
-class _MyAppState extends State<MyApp> {
-  late String _backgroundImage = '';
-
-  @override
-  void initState() {
-    super.initState();
-    _setBackgroundImage();
-  }
-
-  void _setBackgroundImage() {
-    var hourNow = int.parse(DateFormat('kk').format(DateTime.now()));
-    if (hourNow >= 23 || hourNow < 4) {
-      setState(() {
-        print("yes");
-        _backgroundImage = 'assets/images/1.jpg';
-      });
-    } else if (hourNow >= 4 && hourNow < 12) {
-      setState(() {
-        _backgroundImage = 'assets/images/2.jpg';
-      });
-    } else if (hourNow >= 12 && hourNow < 17) {
-      setState(() {
-        _backgroundImage = 'assets/images/3.jpg';
-      });
-    } else if (hourNow >= 17 && hourNow < 23) {
-      setState(() {
-        print("yes");
-        _backgroundImage = 'assets/images/4.jpg';
-      });
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'WeatherApp',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage(_backgroundImage),
-          fit: BoxFit.cover,
-        )),
-        child: const HomePage(),
-      ),
+      MaterialPageRoute(builder: (context) => const WeatherDetailsPage()),
     );
   }
 }
--- lib/pages/weather_details_page.dart
+++ lib/pages/weather_details_page.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class WeatherDetailsPage extends StatelessWidget {
+import 'home_page.dart';
+
+class WeatherDetailsPage extends StatefulWidget {
+  const WeatherDetailsPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _WeatherDetailsPageState();
+  }
+}
+
+class _WeatherDetailsPageState extends State<WeatherDetailsPage> {
+  late String _backgroundImage = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _setBackgroundImage();
+  }
+
+  void _setBackgroundImage() {
+    var hourNow = int.parse(DateFormat('kk').format(DateTime.now()));
+    if (hourNow >= 23 || hourNow < 4) {
+      setState(() {
+        print("yes");
+        _backgroundImage = 'assets/images/1.jpg';
+      });
+    } else if (hourNow >= 4 && hourNow < 12) {
+      setState(() {
+        _backgroundImage = 'assets/images/2.jpg';
+      });
+    } else if (hourNow >= 12 && hourNow < 17) {
+      setState(() {
+        _backgroundImage = 'assets/images/3.jpg';
+      });
+    } else if (hourNow >= 17 && hourNow < 23) {
+      setState(() {
+        print("yes");
+        _backgroundImage = 'assets/images/4.jpg';
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Weather Details'),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'WeatherApp',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      body: Center(
-        child: Text('Weather details'),
+      home: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(_backgroundImage),
+          fit: BoxFit.cover,
+        )),
+        child: const HomePage(),
       ),
     );
   }
